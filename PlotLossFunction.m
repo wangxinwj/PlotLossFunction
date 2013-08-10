71ee168bda8819d745f5c0c53674db0d4a0119ef
WANG Xin <xwang.wx@qq.com>
2013-08-10 15:36:07 +0800

    Update PlotLossFunction.m

--- PlotLossFunction.m
+++ PlotLossFunction.m
@@ -1,6 +1,6 @@
-ezplot('(sign(xy)<0)',[-5 3])
+ezplot('(sign(xy)<0)',[-5 3]) % 0-1损失
 hold on
-ezplot('max(0, 1-xy)',[-5 3])
+ezplot('max(0, 1-xy)',[-5 3]) % 铰链损失
 % ezplot('exp(-xy)',[-2 3])
-ezplot('log2(1+exp(-xy))',[-5 3])
+ezplot('log2(1+exp(-xy))',[-5 3]) % 逻辑斯特损失
 xlabel('y f(x)')
